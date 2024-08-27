4f89976b96f88133e578f58a77be176d25571bcd
Diab16 <abdohdiab166@gmail.com>
2024-08-27 18:55:38 +0300

    feat: create IUnitOfWork and UnitOfWork

---
+++ JumiaEcommeceAPI.DataAccess/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using JumiaEcommeceAPI.DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JumiaEcommeceAPI.DataAccess.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IGenericRepository<Seller> Sellers {  get; }
+        IGenericRepository<Review> Reviews { get; }
+        IGenericRepository<ProductImage> ProductImages { get; }
+        IGenericRepository<Product> Products { get; }
+        IGenericRepository<Payment> Payments { get; }
+        IGenericRepository<OrderItem> OrderItems { get; }
+        IGenericRepository<Order> Orders { get; }
+        IGenericRepository<Category> Categorys { get; }
+
+        int Complete();
+
+    }
+}
---
+++ JumiaEcommeceAPI.DataAccess/Repository/UnitOfWork.cs
@@ -0,0 +1,41 @@
+﻿using JumiaEcommeceAPI.DataAccess.Context;
+using JumiaEcommeceAPI.DataAccess.Entities;
+using JumiaEcommeceAPI.DataAccess.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JumiaEcommeceAPI.DataAccess.Repository
+{
+    internal class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+        public IGenericRepository<Review> Reviews { get; private set; }
+        public IGenericRepository<Seller> Sellers { get;private set; }
+        public IGenericRepository<ProductImage> ProductImages { get; private set; }
+        public IGenericRepository<Product> Products { get;private set; }
+        public IGenericRepository<Payment> Payments { get; private set;}
+        public IGenericRepository<OrderItem> OrderItems { get; private set;}
+        public IGenericRepository<Order> Orders { get; private set;}
+        public IGenericRepository<Category> Categorys { get;private set; }
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+            Reviews = new GenericRepository<Review>(_context);
+            Sellers = new GenericRepository<Seller>(_context);
+            ProductImages = new GenericRepository<ProductImage>(_context);
+            Products = new GenericRepository<Product>(_context);
+            Payments = new GenericRepository<Payment>(_context);
+            OrderItems = new GenericRepository<OrderItem>(_context);
+            Orders = new GenericRepository<Order>(_context);
+            Categorys = new GenericRepository<Category>(_context);
+
+        }
+        public int Complete()
+        {
+            return 0;
+        }
+    }
+}
